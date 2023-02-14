f649d4ab61192408733ea9d467b43bf841a3c823
NicolasEngels <nicolasengels17@gmail.com>
2023-02-14 10:38:19 +0100

    add sign in and create account pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
-import Auth from "./Component/Auth/Auth";
+// import Auth from "./Component/Auth/Auth";
+// import Register from "./Component/Register/Register";
+import Home from "./Component/Home/Home";
 import React from 'react';
 import Header from './Component/Header_and_footer/header';
 
 function App() {
   return (
-    
     <div>
-         <Header />
-         <Auth />
+        <Header />
+        <Home />
     </div>
     
   );
--- src/Component/Auth/Auth.jsx
+++ src/Component/Auth/Auth.jsx
@@ -1,39 +1,43 @@
 import React from "react"
 import "./Auth.css"
-import image from "../../Asset/card_and_pack/back.png"
 
 export default function Auth() {
 
-     return (
-  <div> 
-    <div className="Auth-form-container">
-      <img src={image} alt="back" className="back"/>
-         <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign In</h3>
-            <div className="text-center">
-             
-            </div>
-            <div class="input-group">
-  <input required="" type="text" name="text" autocomplete="off" class="input"/>
-  <label class="user-label">First Name</label> <br/><br/>
-  <input required="" type="text" name="text" autocomplete="off" class="input"/>
-  <label class="user-label">Password</label>
-</div>
-            <div className="form-group mt-3">
-              
-            </div>
-            <div className="d-grid gap-2 mt-3"><br/>
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div><br/>
-            <p className="text-center mt-2">
-              Forgot password? <a href="/auth/forgot">Reset</a>
-            </p>
-          </div>
-        </form>
-      </div>
-      </div>
+    return (
+    <div> 
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+
+                    <h3 className="Auth-form-title">Sign In</h3>
+
+                    <div class="input-group">
+                        <div id="username">
+                            <input required="" type="text" name="text" autocomplete="off" class="input"/>
+                            <label class="user-label">Username</label>
+                        </div>
+
+                        <div id="pass">
+                            <input required="" type="password" name="text" autocomplete="off" class="input"/>
+                            <label class="user-label">Password</label>
+                        </div>
+                    </div>
+
+            
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+
+                    <div id="password">
+                        <a href="/auth/forgot">forgot password</a>
+                        <p> - </p>
+                        <a href="/auth/forgot">Reset</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     )
   }
