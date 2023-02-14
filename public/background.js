2acfdd275ba1e123dad2239fc88badc458e38690
NicolasEngels <nicolasengels17@gmail.com>
2023-02-14 14:58:56 +0100

    ajustement

--- public/background.js
+++ public/background.js
@@ -56,7 +56,7 @@ particlesJS('particles-js',
             },
             "move": {
                 "enable": true,
-                "speed": 1.2,
+                "speed": 1,
                 "direction": "none",
                 "random": false,
                 "straight": false,
